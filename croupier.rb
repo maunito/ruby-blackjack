705e003bbbd01eab9825484017a252716ab0b48c
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2021-05-02 03:42:00 +0200

    player can double the bet now and reward/loss system for double is finished. To do is still that player automatically receives a card withiut being aksed, game ends now if player has not enough money to play, double bet is impossible if player has not enough moeny

--- croupier.rb
+++ croupier.rb
@@ -14,19 +14,50 @@ end
 
 def end_game_message(player_score, dealer_score, money, bet)
   if player_score == 21
-    money += (bet * 2.5)
-    "Wow! You won #{bet * 2.5}$ and have now #{money}$ in your account"
+    if $double == true
+      $money += (bet * 4) # 1 (bet) + 1.5 (blackjack) * 1 (double)
+      puts "Wow! You won #{bet * 3}$ and have now #{$money}$ in your account"
+    else
+      $money += (bet * 2.5) # 1 (bet) + 1.5 (blackjack)
+      puts "Wow! You won #{bet * 1.5}$ and have now #{$money}$ in your account"
+    end
   elsif player_score > 21
-    "Over 21, you lost #{bet}$ and have now #{money}$ in your account"
+    if $double == true 
+      puts "Over 21, you lost #{bet *2}$ and have now #{$money}$ in your account"
+    else
+      puts "Over 21, you lost #{bet}$ and have now #{$money}$ in your account"
+    end
   elsif dealer_score > 21
-    money += (bet * 2)
-    "you won #{bet}$ and have now #{money}$ in your account"
+    if $double == true
+      $money += (bet * 3) # 1 (bet) + 1 (win) + 1 (double)
+      puts "you won #{bet * 2}$ and have now #{$money}$ in your account"
+    else
+      $money += (bet * 2) # 1 (bet) + 1 (win)
+      puts "you won #{bet}$ and have now #{$money}$ in your account"
+    end
   elsif player_score == dealer_score
-    'Push'
+    if double == true
+      $money += (bet * 2) # 1 (bet) + 1 (double)
+    else
+      $money += bet
+      puts "Push - You get your bet back (#{bet}$)"
+    end
   elsif player_score > dealer_score 
-    money += (bet * 2) 
-    "you won #{bet}$ and have now #{money}$ in your account"
+    if $double == true
+      $money += (bet * 3) # 1 (bet) + 1 (win) + 1 (double)
+      puts "you won #{bet * 2}$ and have now #{$money}$ in your account"
+    elsif
+      $money += (bet * 2) # 1 (bet) + 1 (win)
+      puts "you won #{bet}$ and have now #{$money}$ in your account"
+    end
   elsif player_score < dealer_score && dealer_score <= 21
-    "You lost #{bet}$ and have now #{money}$ in your account"
+    if $double == true
+      puts "You lost #{bet * 2}$ and have now #{$money}$ in your account"
+    else
+      puts "You lost #{bet}$ and have now #{$money}$ in your account"
+    end
+  end
+  3.times do
+    sleep_and_line_break(1)
   end
 end
--- interface.rb
+++ interface.rb
@@ -1,6 +1,6 @@
 require_relative 'croupier'
 require_relative 'black_jack'
-money = 50
+$money = 50
 bet = 5
 
 J = 10
@@ -10,30 +10,30 @@ A = 11
 
 values = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
 
-until money <= 0 do
-  puts "Welcome to Blackjack. Your balance is #{money}$"
+until $money <= 4.99 do
+  puts "Welcome to Blackjack. Your balance is #{$money}$"
   puts 'press enter to play (5$)'
   game_start = gets.chomp
   if game_start != "haahsjkfn"
-    money -= bet
+    $money -= bet
     sleep_and_line_break(0.5)
 
     dealer_score = values.sample
 
-    player_score = player_start(values)
+    player_score = player_start(values, bet, $money, $double)
     sleep_and_line_break(0.5)
 
     puts state_of_the_game(player_score, dealer_score)
     sleep_and_line_break(0.5)
 
-    if player_score == 21
+    if player_score == 21 
       puts 'Blackjack!'
     elsif player_score < 21
       puts 'another card? [ y / n ]'
       sleep_and_line_break(0.5)
       answer = gets.chomp
       sleep_and_line_break(0.5)
-      until answer == 'no' || answer == 'n' || player_score >= 21 
+      until answer == 'no' || answer == 'n' || player_score >= 21
         random_card = values.sample
         player_score += random_card
         puts "You pulled a #{random_card}"
@@ -60,6 +60,6 @@ until money <= 0 do
     end
 
 
-    puts end_game_message(player_score, dealer_score, money, bet)
+    end_game_message(player_score, dealer_score, $money, bet)
   end
 end
