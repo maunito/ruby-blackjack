abc82a323daa1f8f25f734cbb74a06f69cf83ea4
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2021-05-08 03:18:57 +0200

    added function which tells the terminal to clear previous games (only the view) to differentiate between game sessions

--- interface.rb
+++ interface.rb
@@ -13,6 +13,8 @@ values = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
 random_card = values.sample
 
 until $money <= 4.99 do
+  system("clear") # nice function which clears terminal history to differentiate game sessions.
+  sleep_and_line_break(0.1)
   new_game # function to display new game banner
   greeting # welcome message and prompt user to start game
   gets.chomp # palyer has to hit enter to continue
