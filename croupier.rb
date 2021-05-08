cd52fcc88ba05de31a8d873de3d72b8384d1475e
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2021-05-08 03:54:58 +0200

    you won or lost banner, credits and minimum bet now visible in new game banner

--- croupier.rb
+++ croupier.rb
@@ -23,18 +23,19 @@
 def new_game
   sleep(0.5)
   puts '----------------------------------------------'
-  puts '                 NEW GAME                     '
+  puts '                   NEW GAME                   '
+  puts "Credit: #{$money}$                        min-bet: 5$"
   puts '----------------------------------------------'
 end
 
 def greeting
-  puts "Welcome to Blackjack. Your balance is #{$money}$"
-  puts '          press enter to play ($)'
+  puts "   Welcome to Blackjack. Your balance is #{$money}$ "
+  puts '            press enter to play ($)'
 end
 
 def game_start(values, random_card)
   bet = 0
-  puts 'how much do you want to bet?'
+  puts 'How much do you want to bet?'
   until bet > 4.99
     bet = gets.chomp.to_i
     sleep_and_line_break(0.5)
@@ -139,6 +140,7 @@ def sleep_and_line_break(n)
 end
 
 def end_game_message(player_score, dealer_score, bet)
+  won = false
   if player_score == 21 && dealer_score != 21
     if $double == true
       $money += (bet * 4) # 1 (bet) + 1.5 (blackjack) * 1 (double)
@@ -147,12 +149,14 @@ def end_game_message(player_score, dealer_score, bet)
       $money += (bet * 2.5) # 1 (bet) + 1.5 (blackjack)
       puts "Wow! You won #{bet * 2.5}$ and have now #{$money}$ in your account"
     end
+    won = true
   elsif player_score > 21
     if $double == true 
       puts "Over 21, you lost #{bet * 2}$ and have now #{$money}$ in your account"
     else
       puts "Over 21, you lost #{bet}$ and have now #{$money}$ in your account"
     end
+    won = false
   elsif dealer_score > 21
     if $double == true
       $money += (bet * 4) # 2 (bet) + 1 (win) + 1 (double)
@@ -161,6 +165,7 @@ def end_game_message(player_score, dealer_score, bet)
       $money += (bet * 2) # 1 (bet) + 1 (win)
       puts "you won #{bet * 2}$ and have now #{$money}$ in your account"
     end
+    won = true
   elsif player_score == dealer_score
     if $double == true
       $money += (bet * 2) # 1 (bet) + 1 (double)
@@ -169,6 +174,7 @@ def end_game_message(player_score, dealer_score, bet)
       $money += bet
       puts "Push - You get your bet back (#{bet}$)"
     end
+    won = nil
   elsif player_score > dealer_score
     if $double == true
       $money += (bet * 4) # 2 (bet) + 1 (win) + 1 (double)
@@ -177,18 +183,41 @@ def end_game_message(player_score, dealer_score, bet)
       $money += (bet * 2) # 1 (bet) + 1 (win)
       puts "you won #{bet * 2}$ and have now #{$money}$ in your account"
     end
+    won = true
   elsif player_score < dealer_score && dealer_score <= 21
     if $double == true
       puts "You lost #{bet * 2}$ and have now #{$money}$ in your account"
     else
       puts "You lost #{bet}$ and have now #{$money}$ in your account"
     end
+    won = false
+  end
+
+  if won == true
+    you_won
+  elsif won == false
+    you_lost
+  else # won == nil (push)
+    you_pushed
   end
   gets.chomp
+
   if $money < 5
     puts '----------------------------------------------'
     puts '                GAME OVER                     '
     puts '----------------------------------------------'
     gets.chomp
   end
+end
+
+def you_won
+  puts '----------------------------------------------'
+  puts '                   YOU WON                    '
+  puts '----------------------------------------------'
+end
+
+def you_pushed
+  puts '----------------------------------------------'
+  puts '                   PUSH                       '
+  puts '----------------------------------------------'
 end
